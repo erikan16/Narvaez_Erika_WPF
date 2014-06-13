456b0f9347dbeecc9c983ce19cfec636fa334983
erikan16 <erikan16@gmail.com>
2014-06-13 02:38:56 -0400

    Update script.js
    Added 3 commits

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,7 +1,8 @@
 // Erika Narvaez
+// Here you can add what time was spent on each project
 var x = prompt("We need to track the amount of time we spend daily on our projects to bill our clients accordingly. How much time did you spend on X project today?");
-var y = prompt("How much time did you spend on Y project today?");
-var fee = prompt("Enter your hourly fee.");
+var y = prompt("How much time did you spend on Y project today?"); // Here you can add what time was spent on each project
+var fee = prompt("Enter your hourly fee.");// this will calculate your hourly fee
 
 var day = parseInt(x) + parseInt(y) * fee;// adding time worked + hourly fee
 
