8294d11d161b5cd7509623d193bdc35a422ee811
erikan16 <erikan16@gmail.com>
2014-06-19 21:28:44 -0400

    Conditional Wacky
    setting up alert and prompts

--- Narvaez_Erika_Conditional_Assignment/Conditionals_Wacky/js/script.js
+++ Narvaez_Erika_Conditional_Assignment/Conditionals_Wacky/js/script.js
@@ -3,3 +3,12 @@
  * June 15, 2014
  * Conditionals Wacky
  */
+
+//alert before prompt begins
+alert("Let's calculate your monthly mortage!");
+
+// receives users information
+var name = prompt("What is your name?");
+
+//alert confirming users name 
+alert("Hi "+name); 
