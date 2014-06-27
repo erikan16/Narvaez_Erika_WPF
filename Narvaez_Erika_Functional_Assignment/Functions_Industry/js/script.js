2b8a2cd97e6dd0e4fda4108040e0ce1e2de928d5
erikan16 <erikan16@gmail.com>
2014-06-26 20:25:07 -0400

    Industry Function
    added if else block

--- Narvaez_Erika_Functional_Assignment/Functions_Industry/js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Industry/js/script.js
@@ -5,11 +5,21 @@
  */
 
 //alert the user what the next prompt is for 
-alert("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone");
-console.log("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone");
+alert("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone!");
+console.log("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone!");
 
 var sandwich = prompt("Do you prefer your sandwiches on pita bread or regular bread?");
-console.log("sandwich")
+console.log("sandwich");
+
+if(sandwich === "pita"){
+	
+		alert("Great Pita bread it is!");
+		console.log("Great Pita bread it is!");
+		
+}else{
+		var bread = prompt("Okay! We have white, wheat, honey wheat, or potato bread?");
+	    console.log(bread);	
+}
 
 
 
