5d42db9409ce0f50ad5ccda8a96517837c449940
erikan16 <erikan16@gmail.com>
2014-06-27 19:07:19 -0400

    Wacky Project
    Finished project { I would really like to learn how to calculate the
    quiz results but found it really hard }

--- Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
@@ -16,60 +16,77 @@ if(setUp === "yes"){
 		var quest1 = prompt("What is the name of Florida's capitol?");
 		console.log(quest1);
 
-	if(quest1 === "Tallahassee"){
+		if(quest1 === "Tallahassee"){
+		
 		alert("Great Job!");
 		console.log("Great Job!");
 	
 	}else {
+		 
 		alert("Oooo! Sorry you got that one wrong!");
 		console.log("Oooo! Sorry you got that one wrong!");
+		
 	}	
 
 	var quest2 = prompt("What is the name of New York's capitol?");
 		console.log(quest2);
 
 	if(quest2 === "Albany"){
+		 
 		alert("Great Job!");
 		console.log("Great Job!");
 	
 	}else {
+		
 		alert("Oooo! Sorry you got that one wrong!");
 		console.log("Oooo! Sorry you got that one wrong!");
-
+		
+	}
 		var quest3 = prompt("What is the name of Hawaii's capitol?");
 		console.log(quest3);
 
 		if(quest3 === "Honolulu"){
+			
 		alert("Great Job!");
-		console.log("Great Job!");
+		console.log("Great Job!");	
 	
 	}else {
+		
 		alert("Oooo! Sorry you got that one wrong!");
-		console.log("Oooo! Sorry you got that one wrong!")
-
+		console.log("Oooo! Sorry you got that one wrong!");
+	
+}
 	var quest4 = prompt("What is the name of Illinois capitol?");
 		console.log(quest4);
 
 	if(quest4 === "Springfield"){
+		 
 		alert("Great Job!");
 		console.log("Great Job!");
-	
+		
 	}else {
+		 
 		alert("Oooo! Sorry you got that one wrong!");
-		console.log("Oooo! Sorry you got that one wrong!")
-
+		console.log("Oooo! Sorry you got that one wrong!");
+			
+}
 		var quest5 = prompt("What is the capitol of West Virginia?");
 		console.log(quest5);
 
 	if(quest5 === "Charleston"){
+		 
 		alert("Great Job!");
 		console.log("Great Job!");
 	
 	}else {
+		 
 		alert("Oooo! Sorry you got that one wrong!");
 		console.log("Oooo! Sorry you got that one wrong!");
+}	
+
 		
-	var total =par
+		alert("Thanks for playing "+name+" you did great!");
+		console.log("Thanks for playing "+name+" you did great!");
 		
 }else if(setUp === "no"){
 	
