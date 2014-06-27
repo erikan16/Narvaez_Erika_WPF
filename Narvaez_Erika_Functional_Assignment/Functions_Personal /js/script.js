a745444431bcc4c312305e0f58eea1ff0d002c0f
erikan16 <erikan16@gmail.com>
2014-06-27 13:19:50 -0400

    Functional Personal
    entered comments

--- Narvaez_Erika_Functional_Assignment/Functions_Personal /js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Personal /js/script.js
@@ -3,47 +3,63 @@
  * June 26, 2014
  * Functional Personal
  */
-
+// alert user what prompts are going to be 
 alert("We are going to calculate your total purchase price!");
 console.log("We are going to calculate your total purchase price!");
 
-
+//prompts user to enter a purchase amount
 var shirt = prompt("How much did your shirt cost?");
 console.log(shirt);
+
+//makes user enter a number 
 while(isNaN(shirt) || ""){
 		shirt = prompt("Please enter a number.");
 }
-
+//prompts user to enter a purchase amount
 var pants = prompt("How much did your pants cost?");
 console.log(pants);
 
+//makes user enter a number
 while(isNaN(pants) || ""){
 	pants = prompt("Please enter a number.");
 }
-		
+//prompts user to enter a purchase amount		
 var shoes = prompt("How much did your shoes cost?");
 console.log(shoes);
 
+//makes user enter a number
 while (isNaN(shoes) || ""){
 	shoes = prompt("Please enter a number.");
 }
-
+//prompts user to enter a purchase amount
 var accesories = prompt("How much did your accesories cost?");
 console.log(accesories);
 
+//makes user enter a number
 while (isNaN(accesories) || ""){
 	accesories = prompt("Please enter a number");	
 }
-var result = price(shirt, pants, shoes, accesories);
-console.log(amountTotal);
+// var result = total(shirt, pants, shoes, accesories);
+// console.log(result);
+console.log(typeof(shirt));
 
-function total(shirt, pants, shoes, accesories){
+//function for purchases
+function total(a, b, c, d){
+	a = parseInt(a);
+	b = parseInt(b);
+	c = parseInt(c);
+	d = parseInt(d);
 	
-	var price = parseInt(shirtBuy + pantsBuy + shoesBuy + accesoriesBuy);
+	var price = a + b + c + d;
 	
 	return price;
 }
 
-alert("Your total amount is $"+result);
-console.log("Your total amount is $"+result);
+//variable for function 
+var finalprice = total(shirt, pants, shoes, accesories);
+
+//alerts user final price
+alert("Your total amount is $"+finalprice);
+console.log("Your total amount is $"+finalprice);
+document.write(finalprice);
 
--- Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
@@ -3,3 +3,46 @@
  * June 26, 2014
  * Functional Wacky
  */
+
+function add(a, b){
+	var c = a + b;
+	return c;
+}
+
+function hello(){
+	document.write("hello world");
+}
+
+var result = add(3,4);
+document.write(result);
+hello();
+
+/*function games(){
+	var name = prompt("What is your name?");
+console.log(name);
+
+var setUp = prompt("Hi "+name+" do you want to play a game!");
+console.log(setUp);
+
+if(setUp === "yes"){
+	alert("Great! Let's go!");	
+}else if(setUp === "no"){
+	alert("Awww man! That's okay maybe next time.");
+}
+
+var quest1 = prompt("What is the name of Florida's capitol?");
+console.log(quest1);
+
+var quest2 = prompt("What is the name of New York's capitol?");
+console.log(quest2);
+
+var quest3 = prompt("What is the name of Hawaii's capitol?");
+console.log(quest3);
+
+var quest4 = prompt("What is the name of Illinois capitol?");
+console.log(quest4);
+
+var quest5 = prompt("What is the name of West Virginia capitol?");
+console.log(quest5);
+}*/
+
