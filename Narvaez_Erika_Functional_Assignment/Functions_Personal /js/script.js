6f146ab2cc6533b8d729cb00d28538a3a0d935a3
erikan16 <erikan16@gmail.com>
2014-06-26 23:51:01 -0400

    Personal
    entered while loop

--- Narvaez_Erika_Functional_Assignment/Functions_Personal /js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Personal /js/script.js
@@ -7,18 +7,33 @@
 alert("We are going to calculate your total purchase price!");
 console.log("We are going to calculate your total purchase price!");
 
+
 var shirt = prompt("How much did your shoes cost?");
 console.log(shirt);
+while(isNaN(shirt) || ""){
+		shirt = prompt("Please enter a number.");
+}
 
 var pants = prompt("How much did your pants cost?");
 console.log(pants);
 
+while(isNaN(pants) || ""){
+	pants = prompt("Please enter a number.");
+}
+		
 var shoes = prompt("How much did your shoes cost?");
 console.log(shoes);
+
+while (isNaN(shoes) || ""){
+	shoes = prompt("Please enter a number.");
+}
 
 var accesories = prompt("How much did your accesories cost?");
 console.log(accesories);
 
+while (isNaN(accesories) || ""){
+	accesories = prompt("Please enter a number");	
+}
 var amountTotal = total(shirt, pants, shoes, accesories);
 console.log(amountTotal);
 
