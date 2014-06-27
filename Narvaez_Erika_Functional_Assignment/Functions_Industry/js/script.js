b27c35d65215e54981d49e313985a724e7bd47c6
erikan16 <erikan16@gmail.com>
2014-06-26 20:05:11 -0400

    Industry Function
    Inserting my code

--- Narvaez_Erika_Functional_Assignment/Functions_Industry/js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Industry/js/script.js
@@ -3,3 +3,13 @@
  * June 26, 2014
  * Functional Industry
  */
+
+//alert the user what the next prompt is for 
+alert("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone");
+console.log("Office Party and you are the organizer! \nWe need to buy sandwiches for everyone");
+
+var sandwich = prompt("Do you prefer your sandwiches on pita bread or regular bread?");
+console.log("sandwich")
+
+
+
