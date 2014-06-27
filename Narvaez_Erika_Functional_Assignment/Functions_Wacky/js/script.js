efd598ad709002040695d4dfaca67f9c57e69607
erikan16 <erikan16@gmail.com>
2014-06-27 18:29:38 -0400

    Wacky project
    finishing up function

--- Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
+++ Narvaez_Erika_Functional_Assignment/Functions_Wacky/js/script.js
@@ -4,45 +4,76 @@
  * Functional Wacky
  */
 
-function add(a, b){
-	var c = a + b;
-	return c;
-}
-
-function hello(){
-	document.write("hello world");
-}
-
-var result = add(3,4);
-document.write(result);
-hello();
-
-/*function games(){
-	var name = prompt("What is your name?");
+var name = prompt("What is your name?");
 console.log(name);
 
 var setUp = prompt("Hi "+name+" do you want to play a game!");
 console.log(setUp);
 
 if(setUp === "yes"){
-	alert("Great! Let's go!");	
+	alert("Great! Let's go!");
+
+		var quest1 = prompt("What is the name of Florida's capitol?");
+		console.log(quest1);
+
+	if(quest1 === "Tallahassee"){
+		alert("Great Job!");
+		console.log("Great Job!");
+	
+	}else {
+		alert("Oooo! Sorry you got that one wrong!");
+		console.log("Oooo! Sorry you got that one wrong!");
+	}	
+
+	var quest2 = prompt("What is the name of New York's capitol?");
+		console.log(quest2);
+
+	if(quest2 === "Albany"){
+		alert("Great Job!");
+		console.log("Great Job!");
+	
+	}else {
+		alert("Oooo! Sorry you got that one wrong!");
+		console.log("Oooo! Sorry you got that one wrong!");
+
+		var quest3 = prompt("What is the name of Hawaii's capitol?");
+		console.log(quest3);
+
+		if(quest3 === "Honolulu"){
+		alert("Great Job!");
+		console.log("Great Job!");
+	
+	}else {
+		alert("Oooo! Sorry you got that one wrong!");
+		console.log("Oooo! Sorry you got that one wrong!")
+
+	var quest4 = prompt("What is the name of Illinois capitol?");
+		console.log(quest4);
+
+	if(quest4 === "Springfield"){
+		alert("Great Job!");
+		console.log("Great Job!");
+	
+	}else {
+		alert("Oooo! Sorry you got that one wrong!");
+		console.log("Oooo! Sorry you got that one wrong!")
+
+		var quest5 = prompt("What is the capitol of West Virginia?");
+		console.log(quest5);
+
+	if(quest5 === "Charleston"){
+		alert("Great Job!");
+		console.log("Great Job!");
+	
+	}else {
+		alert("Oooo! Sorry you got that one wrong!");
+		console.log("Oooo! Sorry you got that one wrong!");
+		
+	var total =par
+		
 }else if(setUp === "no"){
-	alert("Awww man! That's okay maybe next time.");
+	
+alert("Awww man! That's okay maybe next time.");
+
 }
-
-var quest1 = prompt("What is the name of Florida's capitol?");
-console.log(quest1);
-
-var quest2 = prompt("What is the name of New York's capitol?");
-console.log(quest2);
-
-var quest3 = prompt("What is the name of Hawaii's capitol?");
-console.log(quest3);
-
-var quest4 = prompt("What is the name of Illinois capitol?");
-console.log(quest4);
-
-var quest5 = prompt("What is the name of West Virginia capitol?");
-console.log(quest5);
-}*/
 
